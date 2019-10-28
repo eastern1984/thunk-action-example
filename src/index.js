28f789cf130a34339e95b166c50fc6795f0d02b6
Anatoly <tolikybm@gmail.com>
2019-10-28 15:28:42 +0300

    up

--- src/index.js
+++ src/index.js
@@ -9,18 +9,7 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import reducer from './store/reducer';
 
-const logger = (store) => {
-    return next => {
-        return action => {
-            console.log('[Middleware] = ', action);
-            const result = next(action);
-            console.log('[Middleware] next state', store.getState());
-            return result;
-        };
-    }
-};
-
-const store = createStore(reducer, applyMiddleware(logger, thunk));
+const store = createStore(reducer, applyMiddleware(thunk));
 
 const app = (
     <Provider store={store}>
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,7 +1,6 @@
 import * as actionTypes from './actions/actions';
 
 const initialState = {
-    someVar2: 12,
     msg: null
 };
 
